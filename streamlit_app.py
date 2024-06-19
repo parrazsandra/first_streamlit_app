01347ff750248613bb575de7d7a2880b2d7e2cde
parrazsandra <137023815+parrazsandra@users.noreply.github.com>
2024-06-18 19:07:49 -0700

    Update streamlit_app.py
    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
 streamlit.image(
-df2[0],
+'@"ZENAS_ATHLEISURE_DB"."PRODUCTS"."SWEATSUITS"/red_sweatsuit.png',
 width=400,
 caption= product_caption
 )
